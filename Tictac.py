d0fc273c8627b9c1fd5f27142b044f4536bebe41
Tayara <a_tayara333@hotmail.com>
2016-02-07 09:03:23 +0300

    Create Tictac.py

---
+++ Tictac.py
@@ -0,0 +1,35 @@
+import random
+
+def drawBoard(board):
+
+
+
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
+
+def inputPlayerLetter():
+
+    letter = ''
+    while not (letter == 'X' or letter == 'O'):
+        print('Do you want to be X or O?')
+        letter = input().upper()
+
+    if letter == 'X':
+        return ['X', 'O']
+    else:
+        return['X','O']
+
+def whoGoesFirst():
+    if random.randint(0,1) == 0:
+        return 'computer'
+    else:
+        return 'player'
